4a8673f83c98cf38435d305a13d00942f9ba7593
Alfredo08 <alfredosv08@gmail.com>
2021-12-07 10:38:19 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-app';
+  cakes:any = [{
+    name: "Cake 1",
+    rating: 0,
+    comment: "",
+    id: 1
+  },
+  {
+    name: "Cake 2",
+    rating: 0,
+    comment: "",
+    id: 2
+  },
+  {
+    name: "Cake 3",
+    rating: 0,
+    comment: "",
+    id: 3
+  }] 
+
+  newCake:any = {
+    rating: 0,
+    comment: ""
+  }
+  constructor(){}
+
+  commentHandler( event: any, cakeId: number ): void {
+    event.preventDefault();
+    for( let i = 0; i < this.cakes.length; i ++ ){
+      if( cakeId === this.cakes[i].id ){
+        console.log( this.cakes[i] );
+      }
+    }
+  }
+
+  updateComment( event: any ):void{
+    this.newCake.comment = event.target.value;
+    console.log( this.newCake );
+  }
 }
